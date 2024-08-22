241eb034ba4d15aa6eace970d9dc1ec339a2299b
evacstz <eva.cordeiro@aluno.ce.gov.br>
2024-08-22 20:56:19 -0300

    feat(lang); code update

--- Q03.py
+++ Q03.py
@@ -1,5 +1,7 @@
 import time
 
 for x in range (5,-1,-1):
-        print(x)
-        time.sleep(1)
+    print(f'{x}...')
+    time.sleep(1)
+    continue
+print('Feliz Ano Novo, Hugo!!!🎆')
--- teste.py
+++ teste.py
@@ -1,6 +1,25 @@
-# questão 1
+#⚡ questão 1
 
 # x = 1
 # while x < 11:
 #     print(x)
 #     x += 1
+
+#⚡ questão 2
+
+# x = 1
+# while x < 101:
+#     if x % 2 == 0:
+#         print(x)
+#     x += 1
+
+#⚡ questão 3
+
+# import time
+# x = 5
+# while x >= 0 and x <= 5:
+#     print(f'{x}...')
+#     x -= 1
+#     time.sleep(1)
+#     continue
+# print('Happy New Year!!!🎆')
